65994efc20632a7e7d4c5a3772c9db8e0c24a0d9
prianthon <prianthonsubardio@gmail.com>
2016-12-27 09:05:11 +0700

    role admin app

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,6 @@ Auth::routes();
 Route::get('/home', 'HomeController@index');
 
 // route middleware
-Route::group(['prefix'=>'admin', 'middleware'=>['auth']], function () {
+Route::group(['prefix'=>'admin', 'middleware'=>['auth', 'role:admin']], function () {
   Route::resource('authors', 'AuthorsController');
 });
