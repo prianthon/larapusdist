85084098b269a1883b0ee95ac3fde64d9f3858d6
prianthon <prianthonsubardio@gmail.com>
2016-12-30 16:54:36 +0700

    update method book

--- resources/views/books/_form.blade.php
+++ resources/views/books/_form.blade.php
@@ -26,6 +26,11 @@
   {!! Form::label('cover', 'Jumlah', ['class'=>'col-md-2 control-label']) !!}
   <div class="col-md-4">
     {!! Form::file('cover') !!}
+    @if (isset($book) && $book->cover)
+      <p>
+        {!! Html::image(asset('img/'.$book->cover), null, ['class'=>'img-rounded img-responsive']) !!}
+      </p>
+    @endif
     {!! $errors->first('cover', '<p class="help-block">:message</p>') !!}
   </div>
 </div>
