838d4dc15c807659c53941e4e790ed0f48b2fa0a
prianthon <prianthonsubardio@gmail.com>
2016-12-30 10:05:48 +0700

    bookscontroller route

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,5 @@ Route::get('/home', 'HomeController@index');
 // route middleware
 Route::group(['prefix'=>'admin', 'middleware'=>['auth', 'role:admin']], function () {
   Route::resource('authors', 'AuthorsController');
+  Route::resource('books', 'BooksController');
 });
