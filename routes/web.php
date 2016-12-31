880157d318c1c97eacf85caec42ec9f269253260
prianthon <prianthonsubardio@gmail.com>
2016-12-31 13:09:34 +0700

    guestcontroller add

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
-Route::get('/', function () {
-    return view('welcome');
-});
-Auth::routes();
-Route::get('/home', 'HomeController@index');
+Route::get('/', 'GuestController@index');
 
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
 // route middleware
 Route::group(['prefix'=>'admin', 'middleware'=>['auth', 'role:admin']], function () {
   Route::resource('authors', 'AuthorsController');
